677ee6a9606a55dc4bc2a8b07c4898cbc411b3a7
Nick Spring <nick@thespringfamily.com>
2026-08-26 18:45:47 -0400

    Add NWorld.Map project, interfaces, and design notes
    Added NWorld.Map project targeting .NET 10.0. Updated Plan.txt with design notes for display control, tile, and renderer classes. Introduced IMapRenderer interface and MapRenderComponent class with relevant properties.

---
+++ NWorld.Map/Interfaces/IMapRenderer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NWorld.Map.Interfaces
+{
+    public interface IMapRenderer
+    {
+    }
+}
---
+++ NWorld.Map/Models/MapRenderComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NWorld.Map.Models
+{
+    internal class MapRenderComponent
+    {
+        public required Guid ComponentType { get;set;}
+        public string[] Params { get; set;} = [];
+    }
+}
