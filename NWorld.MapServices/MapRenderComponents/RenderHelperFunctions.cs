4d30ad16b426eaefd70083a7f116e20304756227
Nick Spring <nick@thespringfamily.com>
2026-08-29 11:50:27 -0400

    Add rendering function classes and update render helper
    Added new rendering function classes for map components with async-compatible Render stubs using SkiaSharp. Updated RenderHelperFunctions.cs to map component type GUIDs to renderers and invoke them dynamically. Modified project file to include RenderingFunctions folder.

--- NWorld.MapServices/MapRenderComponents/RenderHelperFunctions.cs
+++ NWorld.MapServices/MapRenderComponents/RenderHelperFunctions.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+using NWorld.MapServices.MapRenderComponents.RenderingFunctions;
 
 namespace NWorld.MapServices.MapRenderComponents
 {
     public static class RenderHelperFunctions
     {
+        private static readonly Dictionary<Guid, Func<SKCanvas, int, int, int, Task>> Renderers = new()
+        {
+            { MapRenderComponentConstants.Empty, RenderEmpty.Render },
+            { MapRenderComponentConstants.Grass, RenderGrass.Render },
+            { MapRenderComponentConstants.Water, RenderWater.Render },
+            { MapRenderComponentConstants.DeepWater, RenderDeepWater.Render },
+            { MapRenderComponentConstants.Swamp, RenderSwamp.Render },
+            { MapRenderComponentConstants.Desert, RenderDesert.Render },
+            { MapRenderComponentConstants.Hover, RenderHover.Render },
+            { MapRenderComponentConstants.Selected, RenderSelected.Render },
+            { MapRenderComponentConstants.Range, RenderRange.Render },
+        };
+
+        public static Task RenderComponent(SKCanvas canvas, int tileSize, int x, int y, Guid componentType)
+        {
+            if (!Renderers.TryGetValue(componentType, out var renderer))
+                throw new ArgumentException($"Unknown component type: {componentType}", nameof(componentType));
+
+            return renderer(canvas, tileSize, x, y);
+        }
     }
 }
---
+++ NWorld.MapServices/MapRenderComponents/RenderingFunctions/RenderDeepWater.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using SkiaSharp;
+
+namespace NWorld.MapServices.MapRenderComponents.RenderingFunctions
+{
+    public static class RenderDeepWater
+    {
+        public static Task Render(SKCanvas canvas, int tileSize, int x, int y)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
